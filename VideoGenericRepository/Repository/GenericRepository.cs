ac892ccbbbf9cdfd97347a80697d0b3af1780da4
FabricioMendes <fabriciofbmendes5@gmail.com>
2025-08-21 00:44:42 -0300

    Criação do Repositorio Generico

---
+++ VideoGenericRepository/Repository/GenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGenericRepository.Context;
+
+namespace VideoGenericRepository.Repository
+{
+    public abstract class GenericRepository<T> : IDisposable where T : class, new()
+    {
+        protected readonly GRContext _context;
+        protected readonly DbSet<T> _dbSet;
+        protected GenericRepository(GRContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _dbSet = _context.Set<T>();
+        }
+
+        public void Dispose() => _context.Dispose();
+    }
+}
